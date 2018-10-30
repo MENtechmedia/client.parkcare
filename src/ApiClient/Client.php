a6bd58ebb50b0a3af8e396fe2af9c2c7d0a21906
Eric Landheer <e.landheer@mentechmedia.nl>
2018-10-30 15:28:12 +0100

    :rainbow: Started from scratch...

--- src/ApiClient/Client.php
+++ src/ApiClient/Client.php
@@ -33,4 +33,20 @@ class Client
 			]
 		]);
     }
+
+    public function get($endpoint) {
+        return $this->call('GET', $endpoint);
+    }
+
+	public function put($endpoint) {
+		return $this->call('PUT', $endpoint, $data, $includes);
+    }
+    
+	public function post($endpoint) {
+		return $this->call('POST', $endpoint, $data, $includes);
+    }
+    
+	public function delete($endpoint) {
+		return $this->call('DELETE', $endpoint);
+	}
 }
