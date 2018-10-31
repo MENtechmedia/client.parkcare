f0576315b1628bc84bbbe7871357f092d0519a34
Eric Landheer <e.landheer@mentechmedia.nl>
2018-10-31 09:30:06 +0100

    :art: Add Exception Class

--- src/ApiClient/Model.php
+++ src/ApiClient/Model.php
@@ -2,18 +2,35 @@
 
 class Model
 {
+    private $client;
     private $endpoint;
     private $attributes;
 
     public function __construct($endpoint, $attributes)
     {
         $this->endpoint = $endpoint;
+        $this->client = Client::getInstance();
     }
 
     public static function all($endpoint)
     {
-        $client = Client::getInstance();
-
-        $client->get($endpoint);
+        return self::getContentsFromResponse($client->get($endpoint));
     }
+
+    public static function byId($endpoint, $id)
+    {   
+        return self::getContentsFromResponse($client->get($endpoint . "/" . $id));
+    }
+
+    public static function create($endpoint, $data = [])
+    {
+        
+    }
+
+    private static function getContentsFromResponse($response)
+    {
+        return $response->getBody()->getContents();
+    }
+
+
 }
